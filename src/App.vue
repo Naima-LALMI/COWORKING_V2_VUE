1c7b9ae879992b8dcb952c3e97e7763775022de0
Naima <naima.lalmi28@gmail.com>
2024-07-24 14:29:22 +0200

    demarrage projet

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <Home  sub-title="Bienvenue"/>
+    
+    </template>
+
+<script setup>
+import Home from'@/components/Home.vue';
+
+const subTitle = "Lorem ipsum dolor sit, amet consectetur adipisicing elit.";
+
+</script>
+
+<style scoped>
+ 
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Coworking à Paris</h1>
+  <p>{{ props.subTitle }}</p>
+ 
+ </template>
+ 
+ <script setup>
+ const props = defineProps(['subTitle'])
+ </script>
+ 
+ <style scoped>
+ </style>
