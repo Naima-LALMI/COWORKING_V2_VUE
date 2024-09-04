8185d4f995179bb55520f63d5588772e326e5405
Naima <naima.lalmi28@gmail.com>
2024-09-04 17:42:56 +0200

    realisation fetch API coworking

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,32 @@
 <template>
-<Header />
-<SpotCard v-for="spot in spots" :key="spot.id" :nameSpot="spot.nom" :adressSpot="spot.adresse" :cpSpot="spot.cp" :villeSpot="spot.ville" :picture="spot.image" :url="spot.lien"/>
-
-
+  <Header />
+  <SpotCard
+    v-for="spot in spots"
+    :key="spot.id"
+    :nameSpot="spot.nom"
+    :adressSpot="spot.adresse"
+    :cpSpot="spot.cp"
+    :villeSpot="spot.ville"
+    :picture="spot.image"
+    :url="spot.web"
+  />
 </template>
 
 <script setup>
-import {ref} from 'vue'
+import { ref } from "vue";
 import Header from "@/components/Header.vue";
 import SpotCard from "./components/SpotCard.vue";
-const spots = ref([
-  { id: 1, nom: 'Husy', adresse: '3 rue des Marins', cp: '75010', ville:'Paris', image :'coworking2.jpg', lien:'https://www.hubsy.fr/'},
-  { id: 2, nom: 'Café Code',adresse: '15 rue des Lisses', cp: '75015', ville:'Paris',image :'coworking3.jpg'},
-  { id: 3, nom: 'Why Work in Paris',adresse: '115 avenues des Sapins', cp: '75001', ville:'Paris' ,image :'coworking4.jpg'}
-])
+const spots = ref([]);
 
-
-
+async function afficherCoworking() {
+  const reponse = await fetch(
+    "https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/coworking-france/records?limit=15&refine=ville%3A%22Paris%22"
+  );
+  const coworkings = await reponse.json();
+  spots.value = coworkings.results;
+  console.log(coworkings.results);
+}
+afficherCoworking();
 </script>
 <Header />
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/components/SpotCard.vue
+++ src/components/SpotCard.vue
@@ -14,6 +14,8 @@
 </template>
 
 <script setup>
+
+
 defineProps([
   "nameSpot",
   "adressSpot",
