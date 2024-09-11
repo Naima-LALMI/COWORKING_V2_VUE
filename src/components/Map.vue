b86562062bafa4aeddd142a0432ad9c2fd3e780e
Naima <naima.lalmi28@gmail.com>
2024-09-11 17:45:58 +0200

    rajout commentaire

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,6 +1,8 @@
+
+<!--MAP.VUE EST LE COMPOSANT ENFANT -->
 <template>
   <div id="map" style="height: 500px;"></div>
-  
+
   <!--Ajouter des marqueurs à partir des données de spots--> 
 </template>
 
@@ -8,8 +10,9 @@
 import { onMounted } from 'vue';
 import L from 'leaflet';
 
-// Définir les props pour recevoir des données dynamiques
-const props = defineProps({
+
+// Recevoir la prop "spots" comme un tableau de données
+defineProps({
   spots: Array
 });
 
@@ -21,12 +24,17 @@ onMounted(() => {
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
   }).addTo(map);
- 
 });
+ // Ajouter des marqueurs pour chaque spot après l'initialisation de la carte
+
+
+
+
 </script>
 
 <style>
 #map {
-  height: 50%;  
+  height: 100%;  
+  width: 85%;
 }
 </style>
