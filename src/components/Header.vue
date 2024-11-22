017830ad8fc9fafd9154d1c8760dba7bbd4a9ead
Naima <naima.lalmi28@gmail.com>
2024-11-22 10:52:46 +0100

    suppression balise i class

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,12 @@
 <template>
   <div class="header">
     <h1>Coworking à Paris</h1>
-      <!-- <label for="name">Recherche un lieu</label> -->
-      <input class="barre" type="text" id="searchInput" placeholder="Recherche ton lieu" />
-      <i class="fas fa-search"></i>
+ 
   </div>
 </template>
 
-<script setup></script>
+<script setup> 
+</script>
 
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Playwrite+CU:wght@100&display=swap');
@@ -26,11 +25,5 @@ h1 {
   color: black;
 }
 
-.barre{
-    border-radius: 1em;
-    text-align: center; 
-    margin-top: 1em;
-    margin-bottom: 1em;
-}
 
 </style>
